661c5c7301938b9964421399ba6cbb5913185041
310-06 <310-06@310-06>
2021-12-08 12:06:33 +0500

    Добавьте файлы проекта.

---
+++ module2pr31s01/module2pr31s01/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace module2pr31s01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();          
+        }
+
+       private void Button_Clicked(object sened,EventArgs e)
+        {
+            App.Current.MainPage = new Page1();
+        }
+    }
+}
